aea7d71814a9654da2e5ba78fec1c0ccdf8236e8
zablon <zablonoigo51@gmail.com>
2026-03-09 08:03:45 +0300

    sales event simulator

---
+++ src/main/java/sales/dev/SalesProducer.java
@@ -0,0 +1,13 @@
+package sales.dev;
+
+
+public class SalesProducer {
+
+    private static final String BOOTSTRAP = "localhost:9095,localhost:9102,localhost:9097";
+    private static final String SCHEMA_REGISTRY = "http://localhost:8081";
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
