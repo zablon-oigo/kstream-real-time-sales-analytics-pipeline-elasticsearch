503ef66c32a6d7e8d834ac74a958ef6e961fcf9b
zablon <zablonoigo51@gmail.com>
2026-03-21 20:38:26 +0300

    generate voucher

---
+++ src/main/java/sales/dev/rewards/VoucherGenerator.java
@@ -0,0 +1,16 @@
+package sales.dev.rewards;
+
+import java.security.SecureRandom;
+
+public class VoucherGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generateVoucher(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+}
